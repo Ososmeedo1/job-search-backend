34d672c58656b0de5c87357c1d1734d3265cdb02
Ososmeedo1 <osamaoriginal77@gmail.com>
2026-01-22 22:24:56 +0200

    test 7

--- DB/connection.js
+++ DB/connection.js
@@ -1,23 +1,11 @@
 import mongoose from "mongoose";
 
-let isConnected = false;
 
 export default async function dbConnection() {
-  if (isConnected) {
-    console.log("Using existing DB connection");
-    return;
-  }
-
   try {
-    const db = await mongoose.connect(process.env.CONNECTION_DB_URI, {
-      serverSelectionTimeoutMS: 5000,
-      socketTimeoutMS: 45000,
-      bufferCommands: false,
-    });
-    isConnected = db.connections[0].readyState === 1;
+    await mongoose.connect(process.env.CONNECTION_DB_URI);
     console.log("DB connected");
   } catch (error) {
     console.log("DB failure", error);
-    throw error;
   }
 }
